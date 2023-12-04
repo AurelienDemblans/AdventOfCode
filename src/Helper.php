df9b56f47ba4f42e7d68c820861cae5c81b4cdc1
DESKTOP-9MTB132\Aurel <demblans.dev@gmail.com>
2023-12-04 20:43:59 +0100

    inital setup

---
+++ src/Helper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AOC;
+
+class Helper
+{
+    public static function hello()
+    {
+        echo 'hello', PHP_EOL;
+    }
+}
---
+++ src/day1.php
@@ -0,0 +1,8 @@
+<?php
+var_dump(getcwd());
+require_once 'vendor\autoload.php';
+
+echo file_get_contents('input\day1.txt');
+
+
+AOC\Helper::hello();
